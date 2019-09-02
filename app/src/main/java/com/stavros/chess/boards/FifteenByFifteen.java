d1f9801d26f92c7fa15b0f486ef60697f20a15f8
Stavros Filippidis <stavros489@hotmail.com>
2019-09-02 19:51:19 +0300

    Everything up until 16x16 is working now

--- app/src/main/java/com/stavros/chess/boards/FifteenByFifteen.java
+++ app/src/main/java/com/stavros/chess/boards/FifteenByFifteen.java
@@ -1,16 +1,64 @@
 package com.stavros.chess.boards;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.stavros.chess.R;
+import com.stavros.chess.logic.KnightTest;
 
 public class FifteenByFifteen extends AppCompatActivity {
+
+    KnightTest knightTest = new KnightTest();
+    TextView moves;
+    public int boardSizeFromMainActivity;
+    boolean startFlag = false;
+    boolean endFlag = false;
+    int[] start = new int[2];
+    int[] target = new int[2];
+
+    public static int checkHowManyTimesEnteredInStartAndEndCoords = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifteen_by_fifteen);
+        Intent mIntent = getIntent();
+
+        boardSizeFromMainActivity = mIntent.getIntExtra("valueFromBoardSizeSpinner", 0);
+    }
+
+    public void startAndEndCoords(View v) {
+        startFlag = true;
+        String buttonText = (String) ((AppCompatButton) v).getText();
+        String[] arrOfStr = buttonText.split(",");
+        if (checkHowManyTimesEnteredInStartAndEndCoords == 0) {
+            start[0] = Integer.parseInt(arrOfStr[0]);
+            start[1] = Integer.parseInt(arrOfStr[1]);
+            Toast.makeText(this, "Starting coords are : " + start[0] + "-" + start[1], Toast.LENGTH_SHORT).show();
+        }
+        if (checkHowManyTimesEnteredInStartAndEndCoords > 0) {
+            target[0] = Integer.parseInt(arrOfStr[0]);
+            target[1] = Integer.parseInt(arrOfStr[1]);
+            Toast.makeText(this, "Target coords are : " + target[0] + "-" + target[1], Toast.LENGTH_SHORT).show();
+            endFlag = true;
+            checkStartOrTarget(startFlag, endFlag, start, target);
+        }
+        checkHowManyTimesEnteredInStartAndEndCoords++;
+
+    }
+
+    public String checkStartOrTarget(boolean startFlag, Boolean endFlag, int[] start, int[] target) {
+        if (startFlag && endFlag) {
+            String coordsReturn = knightTest.calculate(start, target, boardSizeFromMainActivity);
+            Toast.makeText(this, coordsReturn, Toast.LENGTH_SHORT).show();
+            return coordsReturn;
+        }
+        return "startFlag or endFlag not true";
     }
 }
